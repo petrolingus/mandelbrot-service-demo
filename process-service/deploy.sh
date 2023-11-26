2fcd7bc789d14559ccdd2458560b70eba59d46db
petrolingus <petrolingus@gmail.com>
2023-11-26 18:58:15 +0300

    fix

--- process-service/deploy.sh
+++ process-service/deploy.sh
@@ -3,6 +3,7 @@
 kubectl create namespace mandelbrot
 
 kubectl apply -f kubernetes/deployment.yaml -n mandelbrot
-#kubectl apply -f kubernetes/service.yaml -n mandelbrot
+kubectl apply -f kubernetes/service.yaml -n mandelbrot
+kubectl apply -f kubernetes/ingress.yaml -n mandelbrot
 
-kubectl expose deployment process-service --type=LoadBalancer --name=process-service -n mandelbrot
+#kubectl expose deployment process-service --type=LoadBalancer --name=process-service -n mandelbrot
