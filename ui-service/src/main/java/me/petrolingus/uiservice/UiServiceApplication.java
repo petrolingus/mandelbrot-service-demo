1cea611dc37b2d925cd68275bb146c7e148ad2e3
petrolingus <petrolingus@gmail.com>
2023-11-27 12:58:12 +0300

    fix

--- ui-service/src/main/java/me/petrolingus/uiservice/UiServiceApplication.java
+++ ui-service/src/main/java/me/petrolingus/uiservice/UiServiceApplication.java
@@ -2,33 +2,19 @@ package me.petrolingus.uiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RestController
 public class UiServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UiServiceApplication.class, args);
     }
 
-	@GetMapping(value = "/api/v1/mandelbrot", produces = MediaType.IMAGE_PNG_VALUE)
-	public void generateMandelbrotTile(
-			@RequestParam(defaultValue = "128") int size,
-			@RequestParam(defaultValue = "-1") double xc,
-			@RequestParam(defaultValue = "0") double yc,
-			@RequestParam(defaultValue = "2") double scale,
-			@RequestParam(defaultValue = "128") int iterations,
-			@RequestParam(defaultValue = "1") int subdivision
-	) throws IOException {
-
-
-
-	}
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
